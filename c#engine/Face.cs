7bd3dd9ddff95f478f0b59e980752914815872df
Kimonic <aimuaimarengiaye@gmail.com>
2023-09-24 17:24:26 -0700

    Add project files.

---
+++ c#engine/Face.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SharpDX;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace c_engine
+{
+    public struct Face
+    {
+        public int A;
+        public int B;
+        public int C;
+
+        public Vector3 Normal;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
